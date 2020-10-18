1be879059e9bcb624539f551faef7a9f408c9adb
Victor Cordero <victordcordero@gmail.com>
2020-10-18 16:19:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,11 +3,9 @@ require 'pry'
 
 def start_game(player1, player2)
 "Hello #{player1} & #{player2}"
-binding.pry
 end
 
 def play_game(player2)
-  binding.pry
 "#{player1} is better than #{player2}"
 end
 
