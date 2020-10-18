68655ccdb83fd1fbc406822face309c7ed14ddf0
Victor Cordero <victordcordero@gmail.com>
2020-10-18 16:46:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,11 +5,12 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-binding.pry
+
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+binding.pry
 def engage
   puts state_log(date)
   date = generate_star_date
