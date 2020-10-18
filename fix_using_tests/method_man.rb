7307d61c70426f952b8683dc9845608f671efc31
Victor Cordero <victordcordero@gmail.com>
2020-10-18 16:21:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,10 +5,13 @@ def start_game(player1, player2)
 "Hello #{player1} & #{player2}"
 end
 
+player1 = "Rocky"
+player2 = "BullWinkle"
+
 def play_game(player2)
 "#{player1} is better than #{player2}"
 binding.pry
 end
 
-player1 = "Rocky" or "Jay-Z"
-player2 = "BullWinkle" or "Beyonce"
+player1 = "Jay-Z"
+player2 = "Beyonce"
