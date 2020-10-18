d4f9b0705a921f53e5322ce0baecc42ddd673625
Victor Cordero <victordcordero@gmail.com>
2020-10-18 16:14:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,17 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+"Hello #{player1} & #{player2}"
 end
+player1 = "Rocky"
+player2 = "BullWinkle"
 
 def play_game(player2)
-  "#{player1} is better than #{player2}"
+  binding.pry
+"#{player1} is better than #{player2}"
+  binding.pry
 end
+
+player1 = "Rocky"
+player2 = "BullWinkle"
