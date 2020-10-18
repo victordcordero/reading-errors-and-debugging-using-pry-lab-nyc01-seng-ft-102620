bda51917173b7c70b267cb4425a07f64b915b448
Victor Cordero <victordcordero@gmail.com>
2020-10-18 17:03:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
+player1 = ("Rocky","Jay-Z")
+player2 = ("BullWinkle", "Beyonce")
 
 def start_game(player1, player2)
 "Hello #{player1} & #{player2}"
 end
-
-player1 = ("Rocky","Jay-Z")
-player2 = ("BullWinkle", "Beyonce")
 
 def play_game(player2)
 "#{player1} is better than #{player2}"
