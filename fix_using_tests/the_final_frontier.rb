7a3d3d9ffe918fb0b3b3c4c381e839fa053d905e
Victor Cordero <victordcordero@gmail.com>
2020-10-18 16:59:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def generate_star_date(date)
-  (rand(100000) + 400000) / 10.0
+def generate_star_date
+  (rand(100000) + 400000) / 10.0 = date
 end
 
 
