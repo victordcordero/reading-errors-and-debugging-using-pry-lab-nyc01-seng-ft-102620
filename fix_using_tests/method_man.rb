d07c249f270cff25bff2b90968cf13916806bad3
Victor Cordero <victordcordero@gmail.com>
2020-10-18 17:03:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,7 +2,7 @@
 require 'pry'
 
 player1 = ("Rocky","Jay-Z")
-player2 = ("BullWinkle", "Beyonce")
+player2 = ("BullWinkle","Beyonce")
 
 def start_game(player1, player2)
 "Hello #{player1} & #{player2}"
